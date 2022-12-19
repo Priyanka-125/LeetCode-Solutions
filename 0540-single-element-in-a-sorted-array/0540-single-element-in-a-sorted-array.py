49801cc20b6c65c09fe5b13d31ae335c08d10999
Priyanka <100556698+Priyanka-125@users.noreply.github.com>
2022-12-19 21:37:22 +0530

    Time: 230 ms (73.43%), Space: 23.9 MB (15.73%) - LeetHub

--- 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.py
+++ 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.py
@@ -4,5 +4,6 @@ class Solution:
         while i < len(nums) - 1:
             if nums[i] != nums[i + 1]: 
                 return nums[i]
-            else: i += 2
-        return nums[-1]
+            else: 
+                i += 2
+        return nums[-1]        
