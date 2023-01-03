c0983311a1e6316ae860982884b0bea910a5e36b
Priyanka <100556698+Priyanka-125@users.noreply.github.com>
2023-01-03 13:04:19 +0530

    Time: 1329 ms (64.64%), Space: 18.6 MB (12.54%) - LeetHub

---
+++ 0015-3sum/0015-3sum.py
@@ -0,0 +1,23 @@
+class Solution:
+    def threeSum(self, nums: List[int]) -> List[List[int]]:
+        trip=[]
+        nums.sort()
+        for i in range(len(nums)):
+            if i > 0 and nums[i - 1] == nums[i]:
+                continue
+            left=i+1
+            right=len(nums)-1
+            while left<right:
+                currsum=0
+                currsum=nums[i]+nums[left]+nums[right]
+                if currsum==0:
+                    trip.append([nums[i],nums[left],nums[right]])
+                    left=left+1
+                    right=right-1
+                elif currsum < 0:
+                    left += 1 
+                else:
+                    right -= 1
+        res = list(set(tuple(sorted(sub)) for sub in trip))
+        return res
+        
