e446b00541a62ee01f8b2b9a3a19b51edd89458d
Priyanka <100556698+Priyanka-125@users.noreply.github.com>
2023-01-04 22:08:05 +0530

    Time: 1279 ms (50.04%), Space: 27.1 MB (64.12%) - LeetHub

--- 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.py
+++ 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.py
@@ -3,4 +3,14 @@ class Solution:
         nums.sort()
         nums=nums[::-1]
         return nums[k-1]
-        
+    
+        '''
+        heap = nums[:k]
+        print(heap)
+        heapify(heap)                  #smallest to index[0] other element not necessarily arranged in order 
+        print(heap)
+        for n in nums[k:]:
+            heappushpop(heap, n)       #pushes n and pop the largest element
+            print(heap)
+        return heap[0]
+        '''
