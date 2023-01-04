ef5d71014a6894da18f15bace429aa0638e44f49
Priyanka <100556698+Priyanka-125@users.noreply.github.com>
2023-01-04 21:20:42 +0530

    Added solution - LeetHub

---
+++ K-th element of two sorted Arrays - GFG/kth-element-of-two-sorted-arrays.py
@@ -0,0 +1,36 @@
+#User function Template for python3
+
+class Solution:
+    def kthElement(self,  arr1, arr2, n, m, k):
+        arr1.extend(arr2)
+        arr1.sort()
+        return arr1[k-1]
+            
+        
+        
+        
+
+
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+def main():
+
+    T = int(input())
+
+    while(T > 0):
+        sz = [int(x) for x in input().strip().split()]
+        n, m, k = sz[0], sz[1], sz[2]
+        a = [int(x) for x in input().strip().split()]
+        b = [int(x) for x in input().strip().split()]
+        ob = Solution()
+        print(ob.kthElement( a, b, n, m, k))
+
+        T -= 1
+
+
+if __name__ == "__main__":
+    main()
+
+# } Driver Code Ends
