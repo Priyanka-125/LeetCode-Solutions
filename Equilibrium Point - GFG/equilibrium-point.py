81ecbf1595cf70c4c9be3a4644568c3bd7e906c9
Priyanka <100556698+Priyanka-125@users.noreply.github.com>
2023-01-11 19:05:35 +0530

    Added solution - LeetHub

---
+++ Equilibrium Point - GFG/equilibrium-point.py
@@ -0,0 +1,46 @@
+# User function Template for python3
+
+class Solution:
+    # Complete this function
+    
+    #Function to find equilibrium point in the array.
+    def equilibriumPoint(self,A, N):
+        # Your code here
+        leftsum=0
+        totalsum=sum(A)
+        for i,nums in enumerate(A):
+            totalsum-=nums
+            if leftsum==totalsum:
+                return i+1
+            leftsum+=nums
+        return -1
+
+
+
+#{ 
+ # Driver Code Starts
+# Initial Template for Python 3
+
+import math
+
+
+def main():
+
+    T = int(input())
+
+    while(T > 0):
+
+        N = int(input())
+
+        A = [int(x) for x in input().strip().split()]
+        ob=Solution()
+
+        print(ob.equilibriumPoint(A, N))
+
+        T -= 1
+
+
+if __name__ == "__main__":
+    main()
+
+# } Driver Code Ends
