4599787c52466a2dddad91c6be1d88b56eab10e8
Priyanka <100556698+Priyanka-125@users.noreply.github.com>
2023-01-16 18:08:38 +0530

    Time: 326 ms (18.69%), Space: 14.1 MB (13.85%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.py
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.py
@@ -11,3 +11,23 @@ class Solution:
             print(charset)
             res = max(res,r-l+1)
         return res
+    
+    '''
+    l = []
+    count = 0
+    i = 0
+    while (i<len(s)):
+        if s[i] not in l:
+            l.append(s[i])
+            i += 1
+        else:
+            if s[i] == l[0]:
+                count = max(count, len(l))
+                l.pop(0)
+            else:
+                count = max(count, len(l))
+                while s[i] != l[0]:
+                    l.pop(0)
+    count = max(count,len(l))
+    return count
+    '''
