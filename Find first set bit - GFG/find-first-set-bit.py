c5b40ffd5cee4f422feeb466a39a9f74b179201f
Priyanka <100556698+Priyanka-125@users.noreply.github.com>
2023-01-17 17:35:44 +0530

    Added solution - LeetHub

---
+++ Find first set bit - GFG/find-first-set-bit.py
@@ -0,0 +1,56 @@
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+import math
+
+
+
+# } Driver Code Ends
+#User function Template for python3
+
+class Solution:
+    
+    #Function to find position of first set bit in the given number.
+    def getFirstSetBit(self,n):
+        #Your code here
+        if n==0:
+            return 0
+        s=bin(n)
+        s=s.split("b")[1]
+        #print(s)
+        s=s[::-1]
+        s=str(s)
+        #print(s)
+        for i in range(len(s)):
+            #print(s[i])
+            if s[i]=='1':
+                return i+1
+
+#{ 
+ # Driver Code Starts.
+    
+    
+def main():
+    
+    T=int(input())
+    
+    while(T>0):
+        
+        
+        n=int(input())
+        ob=Solution()
+        
+        print(ob.getFirstSetBit(n))
+        
+        
+        T-=1
+    
+    
+
+
+
+
+if __name__=="__main__":
+    main()
+# } Driver Code Ends
