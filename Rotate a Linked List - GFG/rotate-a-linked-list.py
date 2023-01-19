fd83292d2037bca5dc6e85e021d6f5415581d1cd
Priyanka <100556698+Priyanka-125@users.noreply.github.com>
2023-01-19 22:25:45 +0530

    Added solution - LeetHub

---
+++ Rotate a Linked List - GFG/rotate-a-linked-list.py
@@ -0,0 +1,71 @@
+# Your task is to complete this function
+
+'''
+
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+'''
+
+
+class Solution:
+    
+    #Function to rotate a linked list.
+    def rotate(self, head, k):
+        # code here
+        tmp=head
+        while tmp.next is not None:
+            tmp=tmp.next
+        tmp.next=head    
+        for i in range(k-1):
+            head=head.next
+        a=head.next
+        head.next=None
+        head=a
+        return head
+
+
+
+#{ 
+ # Driver Code Starts
+# driver
+
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+class LinkedList:
+    def __init__(self):
+        self.head = None
+        self.tail = None
+    
+    def insert(self,val):
+        if self.head is None:
+            self.head = Node(val)
+            self.tail = self.head
+        else:
+            self.tail.next = Node(val)
+            self.tail = self.tail.next
+
+def printList(n):
+    while n:
+        print(n.data, end=' ')
+        n = n.next
+    print()
+
+if __name__=="__main__":
+    for _ in range(int(input())):
+        n = int(input())
+        arr = [int(x) for x in input().split()]
+        k = int(input())
+        
+        lis = LinkedList()
+        for i in arr:
+            lis.insert(i)
+        
+        head = Solution().rotate(lis.head,k)
+        printList(head)
+# } Driver Code Ends
