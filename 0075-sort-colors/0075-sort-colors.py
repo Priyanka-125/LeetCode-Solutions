0df4076d69c72e1fb36ca1ea5c4416260dab1b6e
Priyanka <100556698+Priyanka-125@users.noreply.github.com>
2023-02-18 18:44:52 +0530

    Time: 37 ms (57.61%), Space: 13.8 MB (54.02%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.py
+++ 0075-sort-colors/0075-sort-colors.py
@@ -19,3 +19,41 @@ class Solution:
         nums.extend(w)
         nums.extend(b)
         
+        '''
+        #include <stdio.h>
+
+int main()
+{
+    int n,nums[100],r=0,b=0,g=0;
+    scanf("%d",&n);
+    for(int i=0;i<n;i++){
+        scanf("%d",&nums[i]);
+        if(nums[i]==0){
+            r+=1;
+        }
+        else if(nums[i]==1){
+            b+=1;
+        }
+        else{
+            g+=1;
+        }
+    }
+    for(int i=0;i<n;i++){
+        if(r>0){
+            nums[i]=0;
+            r-=1;
+        }
+        else if(b>0){
+            nums[i]=1;
+            b-=1;
+        }
+        else{
+            nums[i]=2;
+            g-=1;
+        }
+    }
+    for(int i=0;i<n;i++){
+        printf("%d ",nums[i]);
+    }
+}'''
+        
