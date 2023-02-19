1f5ccec7556498c95c99373feb970e740f414bc8
Priyanka <100556698+Priyanka-125@users.noreply.github.com>
2023-02-19 10:24:30 +0530

    Time: 51 ms (69.26%), Space: 14.3 MB (96.46%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.py
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -1,5 +1,6 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
+        '''
         if len(s)!=len(t):
             return False
         if len(s)==len(t):
@@ -9,11 +10,25 @@ class Solution:
         
         return True
     
-    '''
+    
     def isAnagram(self,a,b):
         if sorted(a)==sorted(b):
             return True
         else:
             return False
     '''
+        res1=dict()
+        res2=dict()
+        for i in s:
+            if i not in res1:
+                res1[i]=1
+            else:
+                res1[i]+=1
+        for i in t:
+            if i not in res2:
+                res2[i]=1
+            else:
+                res2[i]+=1
+        return res1==res2
+        
         
